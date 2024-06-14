c108a085d0a2edd02a2f22fea239383392799988
manojkumar <manojkumarr21@gmail.com>
2024-06-14 21:23:40 +0530

    testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -4,7 +4,7 @@ const bodyParser=require('body-parser');
 const multer = require('multer');
 server.use(bodyParser.json());
 const cors = require('cors');
-const { removeBackground } = require('@masuhajime/background-removal-node');
+const { removeBackground } = require('@imgly/background-removal-node');
 
 
 server.use(cors());
@@ -13,9 +13,7 @@ const upload = multer({ dest: 'images/' });
 
 async function removeImageBackground(imgSource) {
   try {
-    console.log("testing",imgSource);
-      const blob = await removeBackground("https://t4.ftcdn.net/jpg/06/41/41/59/360_F_641415906_NCT4SJznIpq8wmA7O9QyCfUXPMgldX1I.jpg");
-      console.log("testing","testing1");
+      const blob = await removeBackground(imgSource);
       const buffer = Buffer.from(await blob.arrayBuffer());      
       return buffer;
   } catch (error) {
